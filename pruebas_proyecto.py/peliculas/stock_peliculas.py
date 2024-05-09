1838304a82bf71e4cdef860fe6a601bff43fea04
Alesrin <68646511+alesrin@users.noreply.github.com>
2024-05-09 03:23:55 +0200

    corrección 2.0

--- pruebas_proyecto.py/peliculas/stock_peliculas.py
+++
@@ -1,3 +0,0 @@
-def stock_peliculas(self):
-        # Método para obtener el stock disponible de la película.
-        return self.stock
--- pruebas_proyecto.py/secret-key.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Generar una clave secreta aleatoria
-SECRET_KEY = os.urandom(24)
-print(SECRET_KEY.hex())
